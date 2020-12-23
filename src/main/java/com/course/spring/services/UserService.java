25e3d7bcf75e4a594f387b822ba5db05061a0d90
Kaio Giovanni <kaio.giovanni@cyan-agro.com>
2020-12-23 13:03:51 -0300

    Exception handler

--- src/main/java/com/course/spring/services/UserService.java
+++ src/main/java/com/course/spring/services/UserService.java
@@ -1,6 +1,7 @@
 package com.course.spring.services;
 
 import com.course.spring.entities.User;
+import com.course.spring.exceptions.ResourceNotFoundException;
 import com.course.spring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> user = repository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User create(User user){
